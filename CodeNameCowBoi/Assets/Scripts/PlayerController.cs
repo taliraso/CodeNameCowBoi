a7a1e2bdebd0d8b4c312b227e1aac76d1163c7cf
Zoogoogalygoo <zacharyzuluaga98@gmail.com>
2023-02-04 18:04:42 +1100

    Fixed WASD binding + Player collision manager

--- CodeNameCowBoi/Assets/Scripts/PlayerController.cs
+++ CodeNameCowBoi/Assets/Scripts/PlayerController.cs
@@ -13,29 +13,34 @@ public class PlayerController : MonoBehaviour
     public float dashSpeed = 20;
     float userDashSpeed; 
 
+    public float currentLeftRightValue = 0;
+    public float currentForwardBackValue = 0;
+    public float currentDashValue = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         userDashSpeed = dashSpeed;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
 
         //Our current x and z 
         float currentXPos = transform.localPosition.x;
         float currentZPos = transform.localPosition.z;
         float currentYPos = transform.localPosition.y;
-
+        
         //are we pressing button? 1 and -1 Yes, 0 No 
         float xMovement = movement.ReadValue<Vector3>().x;
         float zMovement = movement.ReadValue<Vector3>().z;
         float dashMovement = movement.ReadValue<Vector3>().y;
+
+        currentLeftRightValue = xMovement;
+        currentForwardBackValue = zMovement;
+        currentDashValue = dashMovement;
 
         //Moving the player
         float newXPos = currentXPos + xOffset * xMovement * dashSpeed;
