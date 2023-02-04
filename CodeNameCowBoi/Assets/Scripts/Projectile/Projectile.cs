5ee09c24899b7e590b8be6e3563f1778a5e99a03
Zoogoogalygoo <zacharyzuluaga98@gmail.com>
2023-02-05 10:52:16 +1100

    Projectile fires flat + Sand floor put in backend

--- CodeNameCowBoi/Assets/Scripts/Projectile/Projectile.cs
+++ CodeNameCowBoi/Assets/Scripts/Projectile/Projectile.cs
@@ -7,13 +7,12 @@ public class Projectile : MonoBehaviour
     
     public float lanuchVelocity = 5f; // how fast the projectile will shoot
     public float projectileBoundry = 10f; //position limit that the projectile can go 
-
     
-
 
     private void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * lanuchVelocity);
+        transform.position = new Vector3(transform.position.x, 1, transform.position.z);
 
         if (transform.position.x > projectileBoundry)
         {
