d66b352eb839214dde8f086796af817c715b226a
tazzzy505 <tazscott@gmail.com>
2023-02-05 11:46:00 +1100

    initial health UI pass need to figure out how to attach sprites to unity script

--- CodeNameCowBoi/Assets/Scripts/PlayerCollisionManager.cs
+++ CodeNameCowBoi/Assets/Scripts/PlayerCollisionManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCollisionManager : MonoBehaviour
 {
 
     public int health = 3;
     public GameObject floor;
-
+    public Image[] hearts;
+    public Sprite fullHeart;
     public GameObject[][] projectilePoolObject;
     // Start is called before the first frame update
     void Start()
@@ -18,7 +20,18 @@ public class PlayerCollisionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
